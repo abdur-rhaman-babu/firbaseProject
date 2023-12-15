523b7cdd286d661adda013e4ecc2daf5963c77b1
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2023-12-15 13:50:37 +0600

    update

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,26 +2,13 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { app } from "../Config/firebase";
 import { useState } from "react";
+import FireBaseHome from "../Components/FirebaseHome/FireBaseHome";
 
 
 const App = () => {
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  const [user, setuser] = useState(null)
-  const googleSingUpHandaler =()=>{
-    signInWithPopup(auth, provider)
-    .then((res)=>{
-      console.log(res.user);
-      setuser(res.user)})
-    .catch((error)=>console.log(error.message))
-  }
-
   return (
     <div>
-      <h1>{user?.displayName}</h1>
-      <h2>{user?.email}</h2>
-      <img src={user?.photoURL} alt="" />
-      <button onClick={googleSingUpHandaler}>continue with google</button>
+      <FireBaseHome/>
     </div>
   );
 };
